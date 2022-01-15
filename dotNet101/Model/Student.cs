623f2c05fefd7f9044b197fd03981ce37e3faf4d
Matheus Costa dos Santos <matheus.santos@dcnexxy.net.br>
2022-01-15 20:04:26 -0300

    folder refactoring

--- dotNet101/Model/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dotNet101.Model
-{
-    public class Student
-    {
-        public int StudentId { get; set; }
-        public string Name { get; set; }
-        public string Grade { get; set; }
-    }
-}
